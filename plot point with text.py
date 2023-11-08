08b726bbe9d42a4389d6d66212ccb891df856741
Neha Deshmukh <nehakdeshmukh.5@gmail.com>
2023-11-07 23:07:10 -0400

    Update plot point with text.py

--- plot point with text.py
+++ plot point with text.py
@@ -35,22 +35,27 @@ for i in range(0,X):
     for j in range(0,Y):
         pixAr[i][X//2] = green
         pixAr[Y//2][j] = green
-        
+       
+
 def plot_point(p,q):
     pixAr[(X//2)+p][(Y//2)+q] = red
     
     
 def plot_text(p,q):
-    text = font.render('({},{})'.format(p,q), True, blue)
+    
+    text = font.render('({},{})'.format(p,q), True, white)
     textRect = text.get_rect()
-    textRect.center = ((X//2)+p, (Y//2)+q)
+    textRect.center = ((X//2)+(15+p), (Y//2)+(10+q))
     plot_point(p, q)
-    pixAr.close()
-    gamedisplay.blit(text, textRect)
+    return text,textRect
+
     
 
-plot_text(50,50)  
-
+t,tr = plot_text(0,0)  
+t1,tr1 = plot_text(10,10)  
+pixAr.close()
+gamedisplay.blit(t, tr)
+gamedisplay.blit(t1, tr1)
 # main application loop
 run = True
 while run:
